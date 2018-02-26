16553755f14cd1ebc3008d134414a9cdf4e7a84c
whatFruit <matthew.s.atteberry@gmail.com>
2018-02-26 06:51:01 +0000

    I added two test cases: A basic case that checks to see if a keyword is plainly inputed that it is properly lexed. Then a garbage suffix case that checks that each keywords if properly lexed, even with appended with a wide array of what should be lexed as seperate tokens.

--- token.hh
+++ token.hh
@@ -12,3 +12,18 @@ enum tigerTokens {ARRAY=257,IF,THEN,
 				ELESS,EGREATER,AND,OR,
 				ASSINGMENT,ENDL,STRINGLIT,INTLIT,
 				IDENTIFIER,ERROR};
+/*
+const auto = {"array","if","then",
+			  "else","while","for","to",
+			  "do","let","in","end","of",
+			  "break","nil","function",
+			  "var","type","import","primitive",
+			  "class","extends","methods","new"}
+
+const auto = {(ARRAY,"array"),(IF,"if"),(THEN,"then"),
+				(ELSE,"else"),(WHILE,"while"),(FOR,"for"),(TO,"to"),
+				(DO,"do"),(LET,"let"),(IN,"in"),(END,"end"),(OF,"of"),
+				(BREAK,"break"),(NIL,"nil"),(FUNCTION,"function"),
+				(VAR,"va"),(TYPE,"type"),(IMPORT,"import"),(PRIMITIVE,"primitive"),
+				(CLASS,"class"),(EXTENDS,"extends"),(METHODS,"methods"),(NEW,"new")}
+*/
