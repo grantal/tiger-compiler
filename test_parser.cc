9f930fe0230f06fcee012ece7bbd91f75f04fa02
Alex <AlexV348@gmail.com>
2018-03-15 22:21:11 +0000

    add some keyword tests

--- test_parser.cc
+++ test_parser.cc
@@ -25,4 +25,44 @@ TEST_CASE("test that intlits get parsed","[syntax]") {
     REQUIRE(programNode->toStr().find("32") != std::string::npos);
 }
 
+
+
+TEST_CASE("Test Keywords shows up in ast", "[syntax]") {
+    const std::vector<std::string> keywords = {
+        "nil", "id", "32", "\"string\"", "new", "let", 
+        "+", "-", "*", "/", "=", "<>", "<", ">",
+        "<=", ">=", "&", "|", "assignment", "if",
+        "else", "while", "for", "break",
+        "type", "class", "var", "function", 
+        "primitive", "import" 
+    };
+
+    const std::vector<std::string> statements = {
+        "nil", "id", "32", "\"string\"", "new id", "let in end"
+        "1+1", "1-1", "1*1", "1/1", "1=1", "1<>1", "1<1", "1>1",
+        "1<=1", "1>=1", "1&1", "1|1", "id := 2", "if 1 then 2",
+        "if 1 then 2 else 3", "while 4 do 5", "for i := 1 to 2 do 3", "break",
+        "type id = id", "class id {}", "var id := 1", "function id () = 1", 
+        "primitive id ()", "import \"string\"" 
+    };
+    for(auto i=0; i < static_cast<int>(statements.size());++i){
+        SECTION("Test that keyword \"" + keywords[i] + "\" is in the AST for statement \"" + statements[i] + "\""){
+            auto b = buffman::Buffman(statements[i]);
+            REQUIRE(yyparse() == 0); //zero means it parsed successfully
+            REQUIRE(programNode->toStr().find(keywords[i]) != std::string::npos);
+        }
+    }
+}
+    
+/* These next few tests will be reading from files
+All of these files are from:
+https://www.cs.princeton.edu/~appel/modern/testcases/
+*/
+TEST_CASE("make sure real file test1.tig parses correctly","[syntax]") {
+    yyin = fopen("tiger-programs/test1.tig", "r");
+    auto b = buffman::Buffman(yyin);
+    REQUIRE(yyparse() == 0);
+    fclose(yyin);
+}
+
 } //namespace
