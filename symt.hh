2cec643c01774b5afce8b497cad4d927339e1217
Alex <AlexV348@gmail.com>
2018-03-21 20:01:17 +0000

    add some more to symT

--- symt.hh
+++ symt.hh
@@ -2,14 +2,56 @@
  * api for our symbol tables
  * We have 3 classes. First is TypeT which acts as a set of all of the types of the current
  * scope as string.
- * next is Sym which is a container with the string of its id, an enum of it's kind
- * (variable/function/method) and a string of its type
- * Then we have SymT  which stores the symbols for the current scope
- * also a class 'Scope' that stores a TypeT and a SymT and lets you operate on both
+ * Then we have SymT  which contains a map from an identifier to it's type
+ * also a class 'Scope' that stores a TypeT and 2 SymTs, one for functions and one for variables
  */
 #include <string>
 #include <set>
+#include <map>
 
 namespace tiger {
-    using TypeT = std::set<std::string>;
-}
+
+class Scope {
+  public:
+    using type_t = std::string; 
+    using TypeT = std::set<type_t>;
+    // symbol table is a map from identifiers (strings) to types (strings)
+    // I may need to change sym_val_t later if I need to store more than just the type
+    using sym_id_t = std::string;
+    using sym_val_t = std::string;
+    using SymT = std::map<sym_id_t, sym_val_t>;
+
+    // constructors and destructor
+    Scope() = default;   
+    Scope(const Scope &other) = default;   
+    virtual ~Scope() = default;
+        
+    virtual void insertType(type_t newT) {
+       types_.insert(newT);
+    }
+    virtual void emplaceType(type_t newT) {
+       types_.emplace(newT);
+    }
+    virtual void insertVar(std::pair<sym_id_t,sym_val_t> newV) {
+       vars_.insert(newV);
+    }
+    virtual void emplaceVar(sym_id_t newV, sym_val_t newT) {
+       vars_.emplace(newV, newT);
+    }
+    virtual void insertFunc(std::pair<sym_id_t,sym_val_t> newF) {
+       funcs_.insert(newF);
+    }
+    virtual void emplaceFunc(sym_id_t newF, sym_val_t newT) {
+       funcs_.emplace(newF, newT);
+    }
+
+  private:
+    TypeT types_; 
+    SymT vars_;
+    SymT funcs_;
+};
+
+// default types in tiger
+Scope::TypeT defaultTypes = {"int", "string", "array", "record"};
+
+} // namespace
